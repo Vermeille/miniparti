ff1a855cde96fc75c6fdcdd1a29edcddcc6b1bab
Guillaume "Vermeille" Sanchez <guillaume.v.sanchez@gmail.com>
2024-12-13 11:44:59 +0000

    encode

--- encode.py
+++ encode.py
@@ -1,3 +1,4 @@
+import random
 import torch
 import torchvision
 import torchvision.transforms as TF
@@ -15,7 +16,7 @@ def load_data(SZ):
 
     ds = torchvision.datasets.ImageFolder("FCUNIST/train", transform=tfms)
     dl = torch.utils.data.DataLoader(
-        ds, batch_size=256, shuffle=True, num_workers=16, pin_memory=True
+        ds, batch_size=256, shuffle=False, num_workers=16, pin_memory=True
     )
     return dl
 
@@ -27,17 +28,24 @@ def main():
     device = "cpu"
     dl = load_data(128)
     model = baseline()
-    model.load_state_dict(torch.load(sys.argv[1])[
-                          "model"], map_location=device)
+    model.load_state_dict(torch.load(sys.argv[1], map_location=device)[
+                          "model"])
     model.eval()
     model[1].return_indices = True
 
-    for i, (x, _) in enumerate(dl):
+    for i, (x, y) in enumerate(dl):
         x = x.to(device)
         z = model[0](x * 2 - 1)
         _, indices = model[1](z)
-        for ind in indices:
-            print(" ".join(ind.view(-1).tolist()))
+        for j, (ind, yy) in enumerate(zip(indices, y)):
+            filename = dl.dataset.samples[i * 256 + j][0]
+            print(filename, " ".join([str(yy.item())] + [str(ii) for ii in ind.reshape(-1).tolist()] + [str(random.randint(0, 1024)), str(random.randint(0, 1024))]))
+
+        z = model[0](TF.functional.hflip(x) * 2 - 1)
+        _, indices = model[1](z)
+        for j, (ind, yy) in enumerate(zip(indices, y)):
+            filename = dl.dataset.samples[i * 256 + j][0] + "(mirror)"
+            print(filename, " ".join([str(yy.item())] + [str(ii) for ii in ind.reshape(-1).tolist()] + [str(random.randint(0, 1024)), str(random.randint(0, 1024))]))
 
 
 if __name__ == "__main__":
